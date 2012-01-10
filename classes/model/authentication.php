f4509ac2fc619c4db07a52be142855a471bc1638
Rob McCann <rob@carswellgould.co.uk>
2012-01-10 13:36:16 +0000

    Authentication profile_fields correction

--- classes/model/authentication.php
+++ classes/model/authentication.php
@@ -5,7 +5,7 @@ namespace NinjAuth;
 class Model_Authentication extends \Orm\Model {
 	
 	public static $_properties = array(
-		'id', 'provider', 'uid', 'access_token', 'secret', 'expires', 'refresh_token', 'user_id', 'created_at', 'updated_at'
+		'id', 'provider', 'uid', 'access_token', 'secret', 'expires', 'refresh_token', 'user_id', 'profile_fields', 'created_at', 'updated_at'
 	);
 	
 	protected static $_observers = array(
